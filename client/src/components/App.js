87715f6832e144d0c597004fbf56fe05214fa982
lmilliken <millifly@gmail.com>
2018-12-07 14:45:08 -0700

    Full stack authentication completed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
+import Landing from './Landing';
 
 const Dashboard = () => {
   return <h2>Dashboard</h2>;
@@ -11,23 +14,28 @@ const SurveyNew = () => {
   return <h2>SurveyNew</h2>;
 };
 
-const Landing = () => {
-  return <h2>Landing</h2>;
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
-export default App;
+export default connect(
+  null,
+  actions
+)(App); //this makes all of the actions assigned to App as props so you can call them with this.props. fetchUser()
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
-        <div class="nav-wrapper">
-          <a href="#" class="left brand-logo">
-            Logo
-          </a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li>
-              <a href="sass.html">Google</a>
-            </li>
-            <li>
-              <a href="badges.html">Components</a>
-            </li>
-            <li>
-              <a href="collapsible.html">JavaScript</a>
-            </li>
+        <div className="nav-wrapper">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo">
+            WeTeam
+          </Link>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -25,4 +40,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+//get the state{auth: reducers } from reducers/index
+const mapStateToProps = ({ auth }) => {
+  return { auth };
+};
+export default connect(mapStateToProps)(Header);
